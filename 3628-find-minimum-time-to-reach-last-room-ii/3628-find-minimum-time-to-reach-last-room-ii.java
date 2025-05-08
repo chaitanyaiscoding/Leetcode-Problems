0e920164804ebc92d24fcb40a246e9fb83bf8816
Chaitanya challa <110287661+chaitanyaiscoding@users.noreply.github.com>
2025-05-08 08:57:56 +0530

    Time: 1485 ms (5.29%), Space: 111.9 MB (40.97%) - LeetHub

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int minTimeToReach(int[][] moveTime) {
+        int n=moveTime.length;
+        int m=moveTime[0].length;
+        int[][] directions={{-1,0},{0,1},{0,-1},{1,0}};
+        int[][] vis=new int[n][m];
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+
+        pq.add(new int[]{0,0,0,0});
+        vis[0][0]=1;
+        int gg=0;
+        while(true)
+        {
+            int[] curr=pq.poll();
+            // System.out.println(curr[3]);
+            int time=curr[0];
+            int x=curr[1];
+            int y=curr[2];
+            int steps=curr[3];
+            int newsteps=steps+1;
+            // System.out.println(time+" "+x+" "+y);
+            if(x==n-1 && y==m-1)return time;
+
+            for(int[] dir:directions)
+            {
+                int nx=x+dir[0];
+                int ny=y+dir[1];
+
+                if(nx>=0 && nx<n && ny>=0 && ny<m && vis[nx][ny]==0)
+                {
+                    
+                    vis[nx][ny]=1;
+                    System.out.println(gg);
+                    if(moveTime[nx][ny]>time)
+                    pq.add(new int[]{moveTime[nx][ny]+1+((newsteps-1)%2),nx,ny,newsteps});
+                    else
+                    pq.add(new int[]{time+1+((newsteps-1)%2),nx,ny,newsteps});
+
+                }
+            }
+            // gg=gg+1;
+            // System.out.println("gg->"+gg);
+        }
+
+        // return -1;
+    }
+}
