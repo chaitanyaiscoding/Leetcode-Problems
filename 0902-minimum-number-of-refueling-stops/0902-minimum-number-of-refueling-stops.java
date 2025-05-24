62eb0945568e8a256ae5010eeb82b33b66b8c4ed
Chaitanya challa <110287661+chaitanyaiscoding@users.noreply.github.com>
2025-05-24 11:23:46 +0530

    Time: 5 ms (17.18%), Space: 44.9 MB (70.93%) - LeetHub

---
+++ 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        
+        int n=stations.length;
+        if(n==0)
+        {
+            if(target<=startFuel)return 0;
+        }
+        int[] dp=new int[n+1];  
+
+        dp[0]=startFuel;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                if(dp[j]>=stations[i][0])
+                {
+                    dp[j+1]=Math.max(dp[j+1],dp[j]+stations[i][1]);
+                }
+            }
+        } 
+        for(int i=0;i<n+1;i++)
+        {
+            if(dp[i]>=target)
+            return i;
+        }
+        return -1;
+    }
+}
